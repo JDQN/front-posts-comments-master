5990fd0e7c3ee1e57612b65ac9d57d5480e27e50
Sofia Idarraga <sofia.idarraga@udea.edu.co>
2022-09-26 20:48:59 -0500

    preparing ws to future ocation

--- src/app/post-detail/post-detail.component.ts
+++ src/app/post-detail/post-detail.component.ts
@@ -70,7 +70,7 @@ export class PostDetailComponent implements OnInit {
 
         // this.myTimer =  setInterval(() => {
         //    this.closeSocketConnection();
-        //    this.connectToMainSpace()
+        //    this.connectToChannel(this.post ? this.post.aggregateId : 'mainSpace')
         //  }, this.seconds * 1000);
 
       }
