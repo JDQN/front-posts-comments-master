2ff0bd8f6c999106066a09684280a56e4d974239
JuanC <camiloflstudio@gmail.com>
2022-09-28 17:42:38 -0500

    Configuracion variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
+  urlCommands: "https://puntoycoma-proyecto-alpha.herokuapp.com/",
+  urlQuerys: "https://puntoycoma-beta.herokuapp.com/",
+  urlSocket: "ws://puntoycoma-gama.herokuapp.com/",
   firebase: {
     projectId: 'loginpuntocoma-e8c83',
     appId: '1:242290093904:web:a60a53c58cc512231b4655',
